eaa1555d421f84692542253947824f917ccf953c
Adam Drewery <adam.drewery@audacia.co.uk>
2018-11-29 16:02:42 +0000

    Merged PR 2355: Make EnumerableExtensions work with more stuff.
    Make EnumerableExtensions work with more stuff.

--- Audacia.Random/Extensions/EnumerableExtensions.cs
+++ Audacia.Random/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Audacia.Random.Extensions
 {
@@ -7,11 +8,11 @@ namespace Audacia.Random.Extensions
     {
         // Prevent concurrency issues
         private static Lazy<System.Random> _random = new Lazy<System.Random>();
+        
+        public static T Random<T>(this IEnumerable<T> source) => source.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
 
-        public static T Random<T>(this ICollection<T> source) => _random.Value.Element(source);
-
-        public static IEnumerable<T> Random<T>(this IList<T> source, int count) => _random.Value.Elements(source, count);
-
+        public static IEnumerable<T> Random<T>(this IEnumerable<T> source, int count) => source.OrderBy(x => Guid.NewGuid()).Take(count);
+        
         public static IEnumerable<T> Random<T>(this IList<T> source, int min, int max) => _random.Value.Elements(source, min, max);
     }
 }
